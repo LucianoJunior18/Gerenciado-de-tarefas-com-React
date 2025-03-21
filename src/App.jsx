2b474e45471645c640b1aba9bf54092b9733fb20
junior <junioristylo@gmail.com>
2025-03-21 13:13:34 -0300

    colocando para adcionar as tarefas com o titulo e a descriçao ,apagando o imput quando adcionar

--- src/App.jsx
+++ src/App.jsx
@@ -42,6 +42,17 @@ export default function App() {
     setTasks(newTasks)
   }
 
+  function onAddTaskSubmit(title, description) {
+    const newTask = {
+      id: tasks.length + 1,
+      title,
+      description,
+      isCompleted: false
+
+    };
+    setTasks([...tasks, newTask])
+  }
+
   return (
 
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
@@ -50,7 +61,7 @@ export default function App() {
         <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de Tarefas
         </h1>
 
-        <AddTask />
+        <AddTask onAddTaskSubmit={onAddTaskSubmit} />
 
         <Tasks tasks={tasks} onTaskClick={onTaskClick} onDeleteTaskClick={onDeleteTaskClick} />
 
--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,12 +1,31 @@
-export default function AddTask() {
+import { useState } from "react"
+
+export default function AddTask({ onAddTaskSubmit }) {
+
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+
     return (
         <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
-            <input type="text" placeholder="Digite o titulo da tarefa"
-                className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md bg-slate-50 font-medium" />
-            <input type="text" placeholder="Digite a descriçâo da tarefa"
-                className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md bg-slate-50 font-medium" />
+            <input type="text"
+                placeholder="Digite o titulo da tarefa"
+                className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md bg-slate-50 font-medium"
+                value={title}
+                onChange={(event) => setTitle(event.target.value)}
+            />
 
-            <button className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium cursor-pointer">Adcionar</button>
+
+            <input type="text" placeholder="Digite a descriçâo da tarefa"
+                className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md bg-slate-50 font-medium"
+                value={description}
+                onChange={(event) => setDescription(event.target.value)}
+            />
+
+            <button onClick={() => {
+                onAddTaskSubmit(title, description)
+                setTitle("")
+                setDescription("")
+            }} className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium cursor-pointer" value={description}>Adcionar</button>
         </div>
     )
 }
