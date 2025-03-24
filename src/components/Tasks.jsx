4f94982ceeb6e823f6181d5b0bcc7613e9b7c72c
junior <junioristylo@gmail.com>
2025-03-24 14:14:34 -0300

    adcionando uma pagina na tarefa que mostre a descriçao e o titulo ao clicar no botao

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,25 +1,35 @@
 import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 export default function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
+    const navigate = useNavigate();
+
+    function onSeeDetailsClick(task) {
+        const query = new URLSearchParams();
+        query.set("title", task.title)
+        query.set("description", task.description)
+        navigate(`/task?${query.toString()}`);
+    }
 
     return (
         <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
-            {tasks.map((tasks) =>
-                <li key={tasks.id} className="flex gap-2" >
-                    <button onClick={() => onTaskClick(tasks.id)} className={`bg-slate-400 w-full text-left text-white p-2 rounded-md cursor-pointer ${tasks.isCompleted && "line-through"}`}>{
-                        tasks.title}
-
+            {tasks.map((task) => ( // Alterei de `tasks` para `task` aqui
+                <li key={task.id} className="flex gap-2">
+                    <button onClick={() => onTaskClick(task.id)} className={`bg-slate-400 w-full text-left text-white p-2 rounded-md cursor-pointer ${task.isCompleted && "line-through"}`}>
+                        {task.title}
                     </button>
 
-                    <button className="bg-slate-400 p-2 rounded-md text-white cursor-pointer">
+                    <button
+                        onClick={() => onSeeDetailsClick(task)} // Passando `task` aqui
+                        className="bg-slate-400 p-2 rounded-md text-white cursor-pointer">
                         <ChevronRightIcon />
                     </button>
 
-                    <button onClick={() => onDeleteTaskClick(tasks.id)} className="bg-slate-400 p-2 rounded-md text-white cursor-pointer">
+                    <button onClick={() => onDeleteTaskClick(task.id)} className="bg-slate-400 p-2 rounded-md text-white cursor-pointer">
                         <TrashIcon />
                     </button>
                 </li>
-            )}
+            ))}
         </ul>
-    )
+    );
 }
